3243c852395a51bb19c20fad6d3ae11f49840d3a
Arielle-CodeStudio <arielle.codestudio@gmail.com>
2025-05-08 17:25:43 -0400

    Added Header & Java Stats

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
- function userScroll() {
+function userScroll() {
   const navbar = document.querySelector('.navbar');
   const toTopBtn = document.querySelector('#to-top');
 
@@ -13,5 +13,35 @@
   });
 }
 
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function incrementStats() {
+  const counters = document.querySelectorAll('.counter');
+
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute('data-target');
+      const c = +counter.innerText;
+
+      const increment = target / 200;
+
+      if (c < target) {
+        counter.innerText = Math.ceil(c + increment);
+        setTimeout(updateCounter, 1);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
 // Event Listeners
 document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', incrementStats);
